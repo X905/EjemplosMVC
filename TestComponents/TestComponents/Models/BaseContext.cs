20ed37b10241b4e731ec2daa78a04ec7487f7f06
Maria Madelyn Ruiz Claro <maria.ruiz@sv.cds>
2019-03-05 14:48:05 -0600

    Agregué proyecto con el FullCalendar. Falta agregar funcionalidad desde el modelo.

---
+++ TestComponents/TestComponents/Models/BaseContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using TestComponents.Models;
+
+namespace TestComponents.Models
+{
+    public class BaseContext : DbContext
+    {
+        public DbSet<ScheduleModel>Event { get; set; }
+    }
+}
---
+++ TestComponents/TestComponents/Models/ScheduleModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TestComponents.Models
+{
+    public class ScheduleModel
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Order { get; set; }
+        [Required]
+        public string Customer { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime Starts { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime Ends { get; set; }
+
+    }
+}
